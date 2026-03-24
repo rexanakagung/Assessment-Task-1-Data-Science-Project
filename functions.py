1674ffe828e4bd05a32a38434636931e00b7d46d
Rex Anak Agung <rex.anakagung@education.nsw.gov.au>
2026-03-24 21:16:29 +1100

    test retrieving data

--- functions.py
+++ functions.py
@@ -8,6 +8,11 @@ def get_city_info(name):
     url = f"{base_url}/current.json?key={api_key}&q={city_name}&aqi=no" 
     response = requests.get(url) 
     print(response)
+    
+    if response.status_code == 200: 
+        print("Data retrieved!") 
+    else: 
+        print(f"Failed to retrieve data {response.status_code}")
  
 city_name = "London" 
 get_city_info(city_name)
