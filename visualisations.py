ae87d6f4639cf988f4a33c492d249641fa68cb3a
Rex Anak Agung <rex.anakagung@education.nsw.gov.au>
2026-03-26 14:10:34 +1100

    update visualisations.py

--- visualisations.py
+++ visualisations.py
@@ -1,4 +1,16 @@
 def plot_temperature(city_info):
     forecast_days = city_info['forecast']['forecast_days']
     days = [day['day']['maxtemp_c']for day in forecast_days]
-    min_temps = [day['day']['mintemp_c'] for day in forecast_days]
+    min_temps = [day['day']['mintemp_c'] for day in forecast_days] 
+     
+    plt.figure(figsize=(10, 5)) 
+    plt.plot(days, max_temps, marker='o', label='Max Temp (C)', color='tomato')
+    plt.plot(days, max_temps, marker='o', label='Min Temp (C)', color='steelblue')
+    plt.fill_between(days, min_temps, aplha=0.1, color='orange')
+    plt.title(f"7-Day Temperature Forecast - {city_info('location')('name')}")
+    plt.xlabel('Date')
+    plt.ylabel('Temparature (C)')
+    plt.xticks(rotation=45)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
